2d684b5abd37612ba91aafa7b15da05c699fb50b
tulinthecno <127258805+tulinthecno@users.noreply.github.com>
2023-05-02 18:33:57 +0300

    Add files via upload

---
+++ lang/tr/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Bu kimlik bilgileri kayıtlarımızla eşleşmiyor.',
+    'password' => 'Girilen parola geçersiz.',
+    'throttle' => 'Çok fazla giriş denemesi. :seconds saniye sonra lütfen tekrar deneyin.',
+];
